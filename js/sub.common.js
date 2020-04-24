2c5b09b84e213d4c81218a00c9fc5980a07fdf6f
sua-no <sua953@naver.com>
2020-04-24 19:34:54 +0900

    pushstate

--- js/sub.common.js
+++ js/sub.common.js
@@ -1,19 +1,27 @@
 $(function () {
-    // console.log($('.contents').eq(0).data('tab'));
-    let clickTab;
-    $('.tab').on('click', function () {
+    let clickTab,tabHeight;
+
+    //page tab이동시 page값 변경
+    window.addEventListener('popstate', function () {
+      // historyManager(history.state.page);
+    });
+    $('.tab').on('click', tabContents); //탭 클릭시 해당 article 보여줌
+    function tabContents(){
         clickTab = $(this).attr('value');
         $('.contents').each(function (i) {
-            if (clickTab == $('.contents').eq(i).data('tab')) {
+
+            if (clickTab == $('.contents').eq(i).data('tab')) { //tab의 value값과 contents의 data값 비교
+                tabHeight =  $('.contents').eq(i).height();
+                $('.subContents .inner').css({
+                    height : tabHeight
+                });
                 $('.contents').eq(i).show().siblings().hide();
+                historyManager(idx);
             }
-
         });
-    });
-    // history.pushState({key:idx},'','all');
-
-    // window.onpopstate = function(e){
-    //     //e.state.key
-    //     func(e.state.key)
-    // }
+    }
+    function historyManager(idx){
+        history.pushState({page : idx},'title', 'company.html');
+    }
+    
 });
