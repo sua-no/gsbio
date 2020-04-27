a42eb29a8d5666dfd7429620e203acc2e0a61cb2
sua-no <sua953@naver.com>
2020-04-27 18:10:11 +0900

    postate btn color

--- js/sub.common.js
+++ js/sub.common.js
@@ -1,26 +1,46 @@
 $(function () {
-    let clickTab,tabHeight;
+    let tabHeight;
 
+    $(window).on('scroll', subScrollManager);
     //page tab이동시 page값 변경
     window.addEventListener('popstate', function () {
-      // historyManager(history.state.page);
+
+        tabContents(history.state.page);
     });
+
     $('.tab').on('click', tabContents); //탭 클릭시 해당 article 보여줌
-    function tabContents(){
-        clickTab = $(this).attr('value');
+    function tabContents(clickTab) {
+        if (event.target != window) {
+            clickTab = $(this).attr('value');
+            historyManager(clickTab);
+        }
+        $(this).addClass('active').siblings().removeClass('active'); //버튼 색상 변경
         $('.contents').each(function (i) {
             if (clickTab == $('.contents').eq(i).data('tab')) { //tab의 value값과 contents의 data값 비교
-                tabHeight =  $('.contents').eq(i).height();
+                tabHeight = $('.contents').eq(i).height();
                 $('.subContents .inner').css({
-                    height : tabHeight
+                    height: tabHeight
                 });
                 $('.contents').eq(i).show().siblings().hide();
-                historyManager(idx);
             }
         });
+        console.log(event.target != window);
+
     }
-    function historyManager(idx){
-        history.pushState({page : idx},'title', 'company.html');
+    function historyManager(idx) {
+        console.log(idx);
+        history.pushState({ page: idx }, 'title', '#' + idx);
     }
-    
+
+
+    function subScrollManager() {
+        // console.log($(this).scrollTop());
+        if ($(this).scrollTop() != 0) {
+            $('.header').addClass('active');
+
+        } else {
+            $('.header').removeClass('active');
+
+        }
+    }
 });
