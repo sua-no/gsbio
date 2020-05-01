9cdd977e7bc146564d93d2c8d3eb61c730484364
sua-no <sua953@naver.com>
2020-05-02 01:15:20 +0900

    facility table2

---
+++ js/notice.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.subFaq .subject ul li').on('click',faqSlide);
+    function faqSlide(){
+        $(this).toggleClass('active').siblings().removeClass('active');
+        if($(this).hasClass('active')){
+            $(this).find('.faqContent').slideDown();
+            $(this).siblings().find('.faqContent').slideUp();
+        }else{
+            $(this).find('.faqContent').slideUp();
+        }
+    }
+});
