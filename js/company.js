744646571ec125f800e1da1a061883ffed7dcec9
sua-no <sua953@naver.com>
2020-05-04 00:31:34 +0900

    연혁 스크롤 초기화

--- js/company.js
+++ js/company.js
@@ -1,4 +1,20 @@
 $(function () {
+    const interStart = $('.history2020').offset().top - $(window).innerHeight();
+    let years = 1000,countOnce = true;
 
-    // $('.history2020 h4').text()
+    $(window).on('scroll',countStart); // 스크롤 도착시 숫자 2020까지 커지는 함수
+    function countStart(){
+        if(interStart <= $(this).scrollTop()){
+            if(countOnce){
+                countOnce = false; // 인터벌 함수 한번만 실행
+                const yearCount = setInterval(function(){
+                    years += 2;
+                    $('.history2020 h4').text(years);
+                    if(years == 2020){
+                        clearInterval(yearCount);
+                    }
+                },5);
+            }
+        }
+    }
 });
